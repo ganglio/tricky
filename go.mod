e591af8cb685e35b8edf8ded896386a3b683e387
Roberto Torella <robertot@eu.square-enix.com>
2020-03-20 22:46:34 +0000

    Pretty crude but working

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module tricky
 
 go 1.14
+
+require github.com/ganglio/memo v0.0.0-20200221151722-5d509fe2cb45
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/ganglio/memo"
+)
+
+type data struct {
+	Classes []string            `json:"classes"`
+	Words   map[string][]string `json:"words"`
+}
+
+var (
+	words  data
+	page   func() interface{}
+	kitten []byte
+)
+
+func init() {
+	js, err := os.Open("words.json")
+	if err != nil {
+		panic(err)
+	}
+	defer js.Close()
+	body, err := ioutil.ReadAll(js)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(body, &words)
+	if err != nil {
+		panic(err)
+	}
+
+	kt, err := os.Open("kitten.png")
+	if err != nil {
+		panic(err)
+	}
+	defer kt.Close()
+	kitten, err = ioutil.ReadAll(kt)
+	if err != nil {
+		panic(err)
+	}
+
+	page = memo.MemoX(func() (interface{}, error) {
+		return template.ParseFiles("page.gohtml")
+	}, time.Second*5)
+}
+
+func main() {
+
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(kitten)
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rand.Seed(time.Now().UnixNano())
+
+		class := words.Classes[rand.Uint32()%uint32(len(words.Classes))]
+		word := words.Words[class][rand.Uint32()%uint32(len(words.Words[class]))]
+
+		log.Printf("Rendering %s %s", class, word)
+
+		data := struct {
+			Class string
+			Word  string
+		}{
+			Class: class,
+			Word:  word,
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		page().(*template.Template).Execute(w, data)
+	})
+
+	log.Println("Listening on port :3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
